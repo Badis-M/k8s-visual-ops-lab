fa63fcfdc3efe81aadb50d8e8628c0936ae65e35
Badis Merakchi <badis.merakchi@gmail.com>
2026-06-19 19:24:27 +0200

    chore: configure custom domain base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/k8s-visual-ops-lab/",
+  base: "/",
   plugins: [react()],
 });
